f522843d741a38161c2f3a0357858191c26dd6ca
Wako Oshiro <e165711@ie.u-ryukyu.ac.jp>
2016-12-11 21:54:08 +0900

    Report5のStep1

---
+++ src/Main/jp.ac.uryukyu.ie.e165711/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by e165711 on 2016/12/11.
+ */
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        int str_len = str.length();
+
+        System.out.println(str_len);
+    }
+
+}
