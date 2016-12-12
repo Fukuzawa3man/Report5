0ad2689700ce1592cedf471e5f50e9946f3f25e5
Wako Oshiro <e165711@ie.u-ryukyu.ac.jp>
2016-12-12 18:14:36 +0900

    Report5のStep3

--- src/Main/jp.ac.uryukyu.ie.e165711/Main.java
+++ src/Main/jp.ac.uryukyu.ie.e165711/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args){
         try {
             String str = "3．14";
-            double value = Double.parseDouble(str);
             int str_len = str.length();
+            double value = Double.parseDouble(str);
             System.out.println(str_len);
             System.out.println(value);
 
